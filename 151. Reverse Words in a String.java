f99f29f6a25e5b034bb154defa19fb7c41ff099c
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-07-24 12:56:12 +0530

    Add files via upload

---
+++ 151. Reverse Words in a String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        s=s.trim();  // remove trailing and leading white spaces
+        String str[]=s.split("\\s+"); //Each part of the string between two consecutive whitespace occurrences will be                                       separated and stored in the array str.
+        StringBuilder sb=new StringBuilder();
+       
+        for(int i=str.length-1;i>=0;i--){
+            sb.append(str[i]);
+            if(i!=0)
+                sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
