644cf3882a15524cbab5d8e919c842a215e82f0c
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-07-26 15:36:33 +0530

    Add files via upload

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode dummyNode=new ListNode();
+        ListNode prevNode=dummyNode;
+        ListNode currNode=head;
+        while(currNode!=null && currNode.next!=null){
+            prevNode.next=currNode.next;
+            currNode.next=prevNode.next.next;
+            prevNode.next.next=currNode;
+            
+            prevNode=currNode;
+            currNode=currNode.next;
+
+        }
+        return dummyNode.next;
+    }
+}
