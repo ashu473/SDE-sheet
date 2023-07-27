b33ae23dc96f3ea174dfe4c6c326e6f13e9f6929
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-07-27 12:03:50 +0530

    Update 268. Missing Number.java

--- 268. Missing Number.java
+++ 268. Missing Number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int sum=(n*(n+1))/2;
-        for(int i:nums){
+        int sum=(n*(n+1))/2;  // find the sum of n natural numbers
+        for(int i:nums){      // subtract the nums from the sum to get the missing number
             sum-=i;
         }
         return sum;
