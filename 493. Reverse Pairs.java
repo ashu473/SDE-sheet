02381d8af3c9cec1c285fe47dd4d0822ca449404
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-07-31 19:28:33 +0530

    Add files via upload

---
+++ 493. Reverse Pairs.java
@@ -0,0 +1,72 @@
+/*  
+class Solution {
+    public int reversePairs(int[] nums) {
+
+    BRUTE FORCE APPROACH but it gives tle in this problem
+    only 33/140 test cases passed
+        int n=nums.length;
+        long count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if((long)nums[i]>(long)2*nums[j]){
+                    count++;
+                }
+            }
+        }
+        return (int)count;
+    }
+}*/
+public class Solution {
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2;
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += countPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);
+        return cnt;
+    }
+
+    private void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int left = low;
+        int right = mid + 1;
+        int index = 0;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index++] = arr[left++];
+            } else {
+                temp[index++] = arr[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[index++] = arr[left++];
+        }
+
+        while (right <= high) {
+            temp[index++] = arr[right++];
+        }
+
+        System.arraycopy(temp, 0, arr, low, temp.length);
+    }
+
+    private int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) arr[i] > 2L * arr[right]) {
+                right++;
+            }
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+}
